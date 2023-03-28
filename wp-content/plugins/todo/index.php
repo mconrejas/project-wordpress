3eaf13d02a1ddf2f22cf67697bde33f265e3b684
checcoy <napalitcherry@gmail.com>
2023-03-28 18:04:06 +0800

    initial commit

---
+++ wp-content/plugins/todo/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name:       Project-Wordpress Plugin
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            John Smith
+*/
+add_filter( 'show_admin_bar', '__return_false' );
